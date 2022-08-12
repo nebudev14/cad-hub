297320b7246cf71d4103f2e0f33599f6d27811ff
warren yun <wyun2006@gmail.com>
2022-08-12 13:14:41 -0400

    basic command ig

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { invoke } from "@tauri-apps/api";
 
 function App() {
+  invoke("greet", { name: "World" }).then((response) => console.log(response));
+
   return (
     <div className="App">
       <header className="App-header">
